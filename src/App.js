f470e368600f52e95d86a3a2ce182ccec85516d1
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-10-01 09:55:26 -0300

    Exercicios 7 a 9 resolvidos

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import About from './components/About';
 import Users from './components/Users';
 
+// Para aplicar o SPA, utilza-se o React Router. A raiz da aplicação precisa estar encapsulada no BrowserRouter pq assim, todos os seus componentes filhos poderão ser roteados com o Route.
+
 class App extends Component {
   render() {
     return (
+      //path é o caminho da url e a prop exact serve para determinar exatidão entre a rota o elemento a ser renderizado.
       <BrowserRouter>
         <Route exact path="/">
           <Home />
@@ -14,10 +17,9 @@ class App extends Component {
         <Route exact path="/about">
           <About />
         </Route>
-        <Route exact path="/users">
-          <Users />
-        </Route>
+        <Route exact path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning"  />} />
       </BrowserRouter>
+      //Como feito acima, o Route possui props que podem ser passadas para o componente que ele encapsula, permitindo criar paramentros para a url como o 'id' e passar props para o componente.
     );
   }
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,17 @@
+// arquivo Users.js
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Users extends Component {
   render() {
+    const { greetingsMessage } = this.props
+    //As props do Route são: history, location e match. Objetos que possuem métodos especiais para manipular informações na página. O parâmetro da url é acessado no match
+    const { id } = this.props.match.params;
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
-        <Link to="/">Home</Link>
+        <p> { greetingsMessage }, My awesome Users component. Parâmetro url: {id} </p>
+        <Link to='/'>Home</Link>
       </div>
     );
   }
